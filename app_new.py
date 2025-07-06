286cc683b6d2e53c94659c7662b3a97f7d118270
Ahad Nadeem <89941072+alphaahad@users.noreply.github.com>
2025-07-06 13:49:44 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Harmony", layout="wide")
 # --- Centered Heading Using HTML ---
 st.markdown("""
     <h2 style='text-align: center; font-weight: 600; margin-top: 20px;'>
-          Welcome to Project Harmony
+                     Project Harmony
     </h2>
 """, unsafe_allow_html=True)
 
